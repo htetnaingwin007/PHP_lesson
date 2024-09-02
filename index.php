0fc34188d1b24a73725ff59612a1d8dae87fc95a
htetnaingwin <hnwhnw14603@gmail.com>
2024-09-02 21:26:00 +0630

    variable

--- index.php
+++ index.php
@@ -1,4 +1,24 @@
 <?php 
     echo "hello world!";
     echo "<h1>Super Friday.</h1>";
+    $language_type = "Sever-side programming language";
+    echo $language_type;
 ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+        echo "<h1>This is paragraph</h1>";
+    ?>
+    <p>Lorem ipsum dolor sit amet consectetur adipisicing elit. Modi, harum suscipit. Tempora fugit laudantium quos mollitia doloribus. Perspiciatis consequatur tenetur maiores, explicabo beatae aliquid qui odit officia atque illo nobis quia suscipit ullam possimus dolor distinctio tempore necessitatibus facilis, corporis est commodi. Sapiente nam mollitia voluptatem neque id consequatur eius. Aut saepe quas incidunt, laboriosam dolorem, maiores aliquam aperiam nesciunt blanditiis cum eveniet quam tempora rerum error perspiciatis ipsam nam, hic iste laborum esse a ratione cupiditate ad distinctio. At cumque excepturi tenetur fuga recusandae unde soluta neque doloribus corrupti ab minus similique, dolore vitae officiis ea animi libero perferendis nulla deleniti voluptate dolorem numquam nostrum reiciendis molestiae. Sint nam fugit, veritatis rem quas esse possimus incidunt, perferendis sit vitae debitis! Laboriosam accusamus magni molestiae quidem exercitationem nemo doloremque unde corrupti nam officiis id, quibusdam dolores at iusto laborum, dignissimos, impedit magnam? Illo incidunt quae cumque. Neque unde nam facilis provident beatae ad et fugiat commodi ab quis ipsa tempore quaerat possimus fugit asperiores, voluptatem esse sunt harum molestias sit eveniet? Assumenda est a laborum maxime, sunt asperiores sed! Maxime nostrum, cum quis voluptatum eos atque quibusdam iure accusamus dolore tempora. Earum rerum aperiam, quae, possimus soluta ratione numquam obcaecati quibusdam recusandae ab enim eum nihil inventore veniam a explicabo quia reprehenderit esse animi architecto nam? Est, similique molestiae nisi eligendi facere deleniti non unde reiciendis excepturi deserunt sapiente hic ducimus repudiandae aspernatur facilis blanditiis voluptates dignissimos! Officiis velit ratione debitis rerum accusantium. Maxime repellendus consequuntur facilis quisquam id deserunt adipisci ducimus laudantium? Dicta vero sapiente cupiditate ipsam ea perspiciatis aspernatur neque hic itaque quaerat reiciendis beatae fuga natus voluptatem nisi perferendis, distinctio accusantium consequuntur, facere, numquam dolores amet atque rerum. In laboriosam laborum quaerat repellendus rem iste eveniet at atque aperiam, consequatur, ratione vitae! Minima vitae perferendis nemo inventore sapiente architecto ab quidem nesciunt rem, cum voluptatibus quae voluptatem officiis facilis, ipsa natus tempore perspiciatis explicabo placeat provident doloremque. Autem itaque similique beatae voluptatum aut voluptatibus, et quas mollitia nostrum natus inventore quaerat illo illum aliquam reprehenderit, alias dolorum non tenetur obcaecati praesentium aliquid sit! Cumque consequatur odio doloribus ratione eligendi placeat culpa quia atque dolore natus voluptatem voluptatibus ipsum perferendis quibusdam repudiandae assumenda in, dignissimos consequuntur consectetur rem aspernatur architecto ipsam, numquam rerum? Maiores beatae vitae repellat non blanditiis alias aspernatur ea et repellendus veritatis veniam odio, suscipit quasi magni, sapiente sint adipisci dolorum nihil exercitationem odit? Voluptas ab eius porro ipsum iure ratione dolores pariatur voluptates quidem saepe reiciendis, fuga fugit iusto sequi possimus voluptatibus numquam eum perferendis repudiandae quas laudantium, rem error. Recusandae pariatur repellat aspernatur tempora, aperiam laboriosam culpa ducimus consequatur molestias voluptates quaerat voluptatum ratione illum temporibus ea enim! Harum ipsa deleniti tempore nulla repellat porro qui distinctio praesentium maiores. Asperiores doloribus, quos blanditiis, aperiam repellat sunt illo magni, doloremque obcaecati nulla eius impedit! Esse, voluptatem eius odit inventore at molestiae iure unde sit odio, et a id voluptate voluptatibus totam nesciunt voluptatum harum temporibus minus laudantium pariatur quasi animi. Excepturi, sapiente. Ipsa, placeat.</p>
+
+    <script>
+        document.write('<h1>I am Javascript</h1>');
+    </script>
+</body>
+</html>
---
+++ variable_and_string.php
@@ -0,0 +1,16 @@
+<?php
+    //variable
+    //Naming rule - no space,no special character !@#$%^*&(),can't be a number on first character,case sensitive
+
+    $name = "Htet Naing Win";
+    $address = "Mandalay";
+
+
+    //double quote and single quote
+    echo "My name is $name";
+    echo "<br>";
+    echo 'I live in $address<br>';
+    echo 'I live in '.$address;
+    echo "<br>";
+    echo $name.' live in '.$address;
+?>
